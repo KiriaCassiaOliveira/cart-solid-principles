8a4e9819f143cab519129348e9fc6620b202fcd5
KiriaCassiaOliveira <kiriacsoliveira@gmail.com>
2024-08-08 11:49:12 -0300

    solid advantages and disadvantages

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+/* vantagens e desvantagens do S.O.L.I.D
+
+Vantagens:
+
+- Código modular
+- Código Reutilizável (D.R.Y - Don't repeat yourself)
+- Código testável, baixo acoplamento
+- alta coesão
+- Código expansível
+- Separations of concerns (Separação de conceitos)
+- Fácil manuntenção
+
+Desvantagens:
+- Complexidade
+- Quantidade de código digitado aumenta
+- Tempo de desenvolvimento aumenta.
+- Tenha cuidados com: YAGNI, KISS (You arent gonna need it, Keep it simple, stupid)
+*/
